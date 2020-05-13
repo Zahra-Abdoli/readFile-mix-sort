97538944f08f1affbc49fa6273de468597979b22
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-05-13 13:40:57 +0200

    done

--- ReadFile.Mix.Sort/Program.cs
+++ ReadFile.Mix.Sort/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.IO;
 
 namespace ReadFile.Mix.Sort
 {
@@ -6,7 +10,36 @@ namespace ReadFile.Mix.Sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines1 = File.ReadAllLines(@"C:\Users\Svea User\Source\Repos\ReadFile.Mix.Sort\TextFile1.txt");
+            var lines2 = File.ReadAllLines(@"C:\Users\Svea User\Source\Repos\ReadFile.Mix.Sort\TextFile2.txt");
+
+            string[] field = new string[20];
+            int fieldIndex = 0;
+            for (int i = 0; i < lines1.Length; i++)
+            {
+                string[] lines = lines1[i].Split(' ');
+                foreach (var item in lines)
+                {
+                    field[fieldIndex] = item;
+                    fieldIndex++;
+                }
+            }
+            for (int i = 0; i < lines2.Length; i++)
+            {
+                string[] lines = lines2[i].Split(' ');
+                foreach (var item in lines)
+                {
+                    field[fieldIndex] = item;
+                    fieldIndex++;
+                }
+            }
+
+            Array.Sort(field);
+            foreach (string item in field)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
+
 }
